637e7badeef04c588c597a2ed7c8508c1c5d8d7e
pralamo <pralamo@.pruebas.es>
2024-08-13 07:04:35 +0200

    Creamos componentes. Agregamos bootstrap. Listos para crear formularios.

---
+++ src/components/Contador.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1 class="text-center">Hola Pablo</h1>
+  <h2 class="text-center">Contador.vue desde components</h2>
+  <div class="text-center">
+    <span class="border border-primary fs-1 text-bg-primary">&nbsp;&nbsp;{{ contador }}&nbsp;&nbsp;</span>
+  </div>
+  <div class="text-center">
+    <button type="button" class="btn btn btn-outline-primary" @click="incrementar">
+      Incrementar
+    </button>
+    <button type="button" class="btn btn-outline-primary" @click="decrementar">
+      decrementar
+    </button>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from "vue";
+  const contador = ref(0);
+  const incrementar = () => {
+    contador.value++;
+  };
+  const decrementar = () => {
+    contador.value--;
+  };
+</script>
+
+<style scoped>
+  .contador {
+    color: blue;
+    font-size: large;
+  }
+
+  button {
+    padding: 5px;
+    margin: 5px;
+  }
+</style>
